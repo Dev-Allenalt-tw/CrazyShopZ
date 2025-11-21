e1422df625f8b369ea4b7c47e3b985596883174a
Dev-Allenalt-tw <rohitcreationsyt@gmail.com>
2025-11-21 18:07:23 +0530

    Create Permissions.java

---
+++ src/main/me/devallenalt_tw/crazyshopz/utils/Permissions.java
@@ -0,0 +1,9 @@
+package me.devallenalt_tw.crazyshopz.utils;
+
+public class Permissions {
+    
+    public static final String USE = "crazyshopz.use";
+    public static final String SELL = "crazyshopz.sell";
+    public static final String ADMIN = "crazyshopz.admin";
+    
+}
